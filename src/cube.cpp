68a45ade760bf86763f615745c17673338e6007e
Jay Bourque <jayvius@gmail.com>
2024-04-20 21:15:04 -0500

    Add cube

--- src/cube.cpp
+++ src/cube.cpp
@@ -1,51 +1,27 @@
 #include "cube.hpp"
 #include <array>
 
-const size_t RBF = 0;
-const size_t RTF = 1;
-const size_t LTF = 2;
-const size_t LBF = 3;
-const size_t RBB = 4;
-const size_t RTB = 5;
-const size_t LTB = 6;
-const size_t LBB = 7;
-
-void addTriangle(Buffer &buffer, std::array<VertexIndex, 8> &indices, size_t v1, size_t v2, size_t v3)
-{
-    addTriangle(buffer, indices[v1], indices[v2], indices[v3]);
-}
+#define PAR_SHAPES_IMPLEMENTATION
+#include "par_shapes.h"
 
 void addCube(Buffer &buffer, glm::mat4 transform)
 {
-    float vertices[] = {
-        0.5f, -0.5f, 0.5f,   // RBF
-        0.5f, 0.5f, 0.5f,    // RTF
-        -0.5f, 0.5f, 0.5f,   // LTF
-        -0.5f, -0.5f, 0.5f,  // LBF
-        0.5f, -0.5f, -0.5f,  // RBB
-        0.5f, 0.5f, -0.5f,   // RTB
-        -0.5f, 0.5f, -0.5f,  // LTB
-        -0.5f, -0.5f, -0.5f, // LBB
-    };
-
-    std::array<VertexIndex, 8> indices;
-
-    glm::vec4 v;
-    for (auto i = 0; i < 8; i++) {
-        v = glm::vec4(vertices[(i*3)], vertices[(i*3)+1], vertices[(i*3)+2], 1.0f) * transform;
-        indices[i] = addVertex(buffer, {{v[0], v[1], v[2]}, {255, 255, 255, 255}});
+    par_shapes_mesh *cube = par_shapes_create_cube();
+    for (auto i = 0; i < cube->ntriangles * 3; i += 3) {
+        float x, y, z;
+        x = cube->points[3*cube->triangles[i]] - 0.5;
+        y = cube->points[3*cube->triangles[i]+1] - 0.5;
+        z = cube->points[3*cube->triangles[i]+2] - 0.5;
+        VertexIndex v0 = addVertex(buffer, {{x, y, z}, {255, 255, 255, 255}});
+        x = cube->points[3*cube->triangles[i+1]] - 0.5;
+        y = cube->points[3*cube->triangles[i+1]+1] - 0.5;
+        z = cube->points[3*cube->triangles[i+1]+2] - 0.5;
+        VertexIndex v1 = addVertex(buffer, {{x, y, z}, {255, 255, 255, 255}});
+        x = cube->points[3*cube->triangles[i+2]] - 0.5;
+        y = cube->points[3*cube->triangles[i+2]+1] - 0.5;
+        z = cube->points[3*cube->triangles[i+2]+2] - 0.5;
+        VertexIndex v2 = addVertex(buffer, {{x, y, z}, {255, 255, 255, 255}});
+        addTriangle(buffer, v0, v1, v2);
     }
-
-    addTriangle(buffer, indices, RBF, RTF, LTF);
-    addTriangle(buffer, indices, RBF, LTF, LBF);
-    addTriangle(buffer, indices, RBB, RTB, RTF);
-    addTriangle(buffer, indices, RBB, RTF, RBF);
-    addTriangle(buffer, indices, RTF, RTB, LTB);
-    addTriangle(buffer, indices, RTF, LTB, LTF);
-    addTriangle(buffer, indices, LTB, RTB, RBB);
-    addTriangle(buffer, indices, LBB, LTB, RBB);
-    addTriangle(buffer, indices, LTF, LTB, LBB);
-    addTriangle(buffer, indices, LBF, LTF, LBB);
-    addTriangle(buffer, indices, LBB, RBB, RBF);
-    addTriangle(buffer, indices, LBF, LBB, RBF);
+    par_shapes_free_mesh(cube);
 }
